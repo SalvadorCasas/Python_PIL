ee063af13a9831db58148585f3f5fe5a1f646474
SalvadorCasas <salvadorcasas11@gmail.com>
2022-09-01 21:45:39 -0300

    Agrego archivo py

--- clase_1/hola_mundo.py
+++ clase_1/hola_mundo.py
@@ -1,1 +1,14 @@
-print("Hola Mundo");
+#Mostrar datos
+print("Hola Mundo\n") #\n Salto de linea 
+print("Chau\tMundo") #\t Tabulacion en linea
+print("Hola" + "Mundo") #Concatenacion
+print('Hola','Mundo',sep=' ') #Otra manera
+print('Hola','Mundo',end='\n')#Agrega algo al final
+print(2 + 2)
+print(4**2)
+
+#Entrada de datos
+input("Ingrese un Mensaje:\t")
+
+
+
---
+++ clase_1/variables.py
@@ -0,0 +1,10 @@
+#mensaje_usuario = input("Ingrese un mensaje:\t")
+#print(mensaje_usuario)
+
+
+x = int(input("Ingrese un numero"))
+y = int(input("Ingrese otro numero"))
+
+resultado = x + y
+
+print('El resultado es-->',resultado)
